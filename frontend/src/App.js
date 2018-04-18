009052b4a5740b78adfbd9ce00511b89e60ac89f
Sam Chung <chungsam@outlook.com>
2018-04-18 15:49:20 -0700

    Add mapStateToProps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
-import PostList from './components/post/PostList';
-import './App.css';
+import React, { Component } from "react";
+import PostList from "./components/post/PostList";
+import { connect } from 'react-redux'
+import "./App.css";
 
 class App extends Component {
+  state = {
+    posts: []
+  };
+
   render() {
+    const {posts} = this.state;
+
     return (
       <div className="App">
-        <header>
-          Nav here!
-        </header>
-        <PostList />
+        <header>Nav here!</header>
+        <PostList posts={posts} />
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps({ posts }) {
+  return {
+    posts
+  };
+}
+
+export default connect(
+  mapStateToProps
+)(App);
